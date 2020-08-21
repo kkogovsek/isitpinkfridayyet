57eef4ebbc335a26692580e4050d0bd856f1f06b
Klemen Kogovsek <klemen.kogovsek@tretton37.com>
2020-08-21 13:55:28 +0200

    Created it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle, css } from 'styled-components'
+
+const Global = createGlobalStyle`
+  :root {
+    --pink: #e6007e;
+    --black: black;
+
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    position: relative;
+    height: 100vh;
+    ${({pink}) => pink ? css`
+    background: var(--pink);
+    color: var(--black);
+    ` : css`
+    color: var(--pink);
+    background: var(--black);
+
+  `}
+  }
+`
+
+const Text = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 128px;
+  font-weight: bold;
+  font-family: sans-serif;
+`
 
 function App() {
+  const pink = new Date().getDay() === 5;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Global pink={pink}/>
+    {pink && <Text>YES 🥳</Text>}
+    {!pink && <Text>NO 😢</Text>}
+  </>
   );
 }
 
